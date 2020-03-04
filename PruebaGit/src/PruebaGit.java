35f3c3d2641eced2435a314f2f3dce6d77c456af
guilleS <Guille@DESKTOP-53FVRT0>
2020-03-04 13:42:30 +0100

    Constructor y getter y setter

--- PruebaGit/src/PruebaGit.java
+++ PruebaGit/src/PruebaGit.java
@@ -3,6 +3,24 @@ public class PruebaGit {
 
 	private int a;
 	private int b;
+	public PruebaGit(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	
 	
 	
 	
